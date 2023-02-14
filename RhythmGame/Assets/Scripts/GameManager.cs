bba6bc40b953796cc0ef617a6cd143ce4bf3d595
tjdnjsrmsdidgkrID <tjdnjsrmsdidgkrdlapdlf@gmail.com>
2023-02-15 00:25:26 +0900

    Background

--- RhythmGame/Assets/Scripts/GameManager.cs
+++ RhythmGame/Assets/Scripts/GameManager.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public BoxCollider[] d_check_collider = new BoxCollider[4];
-    public BoxCollider[] f_check_collider = new BoxCollider[4];
-    public BoxCollider[] j_check_collider = new BoxCollider[4];
-    public BoxCollider[] k_check_collider = new BoxCollider[4];
+    [Serializable]
+    public class check_collider_1d
+    {
+        public BoxCollider[] check_collider = new BoxCollider[4];
+    }
+
+    public check_collider_1d[] check_collider_2d = new check_collider_1d[4];
+
+    //public BoxCollider[,] check_collider = new BoxCollider[4, 4];
+    //public BoxCollider[] d_check_collider = new BoxCollider[4]; //2차원 배열로 만들어서 CheckNote함수의 인자를 숫자로 할 수 있도록
+    //public BoxCollider[] f_check_collider = new BoxCollider[4];
+    //public BoxCollider[] j_check_collider = new BoxCollider[4];
+    //public BoxCollider[] k_check_collider = new BoxCollider[4];
 
     LayerMask mask;
 
@@ -21,13 +31,13 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.D))
-            CheckNote(d_check_collider);
+            CheckNote(0);
         if (Input.GetKeyDown(KeyCode.F))
-            CheckNote(f_check_collider);
+            CheckNote(1);
         if (Input.GetKeyDown(KeyCode.J))
-            CheckNote(j_check_collider);
+            CheckNote(2);
         if (Input.GetKeyDown(KeyCode.K))
-            CheckNote(k_check_collider);
+            CheckNote(3);
 
         if (Input.GetKeyDown(KeyCode.E))
             SpawnNote(0);
@@ -39,14 +49,14 @@ public class GameManager : MonoBehaviour
             SpawnNote(3);
     }
 
-    void CheckNote(BoxCollider[] check_collider)
+    void CheckNote(int area)
     {
         Collider[] other;
 
-        for (int i = 0; i < check_collider.Length; i++)
+        for (int i = 0; i < 4; i++)
         {
-            other = Physics.OverlapBox(check_collider[i].center,
-                                       check_collider[i].size / 2,
+            other = Physics.OverlapBox(check_collider_2d[area].check_collider[i].center,
+                                       check_collider_2d[area].check_collider[i].size / 2,
                                        Quaternion.identity,
                                        mask);
 
@@ -71,6 +81,8 @@ public class GameManager : MonoBehaviour
 
             return;
         }
+
+        Debug.Log("Nothing"); //4개의 콜라이더를 모두 검사해서 검색된 노트가 없는 경우
     }
 
     void SpawnNote(int area)
